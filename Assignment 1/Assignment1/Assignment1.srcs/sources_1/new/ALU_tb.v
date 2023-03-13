fda66d66ac93c3218f1f3a5de38b9cb2131350f3
Aaron Dinesh <aarondinesh2002@gmail.com>
2023-03-13 19:28:51 +0000

    Added Testbench to Assignment 1 -Created a comprehensive testbench for the ALU module

--- Assignment 1/Assignment1/Assignment1.srcs/sources_1/new/ALU_tb.v
+++ Assignment 1/Assignment1/Assignment1.srcs/sources_1/new/ALU_tb.v
@@ -2,54 +2,109 @@
 
 
 module ALU_tb();
-    reg [5:0] A, B;
+    reg [5:0] A, B, expected;
     reg [3:0] fxn;
     wire [5:0] out;
     ALU dut(.A(A), .B(B), .fxn(fxn), .X(out));
+   
     
     initial begin
-//        //Return A
-//        A = 6'b001000;
-//        B = 6'b000000;
-//        fxn = 3'b000;
-//        #200;
+        //Return A for +ve A. Output should be 6'b001000
+        A = 6'b001000;
+        B = 6'b000000;
+        fxn = 3'b000;
+        expected = 6'b001000;
+        #50;
         
-//        //Return B
-//        A = 6'b001010;
-//        B = 6'b001010;
-//        fxn = 3'b001;
-//        #200;
+        //Return B for +ve B. Output should be 6'b001010
+        A = 6'b010010;
+        B = 6'b001010;
+        fxn = 3'b001;
+        expected = 6'b001010;
+        #50;
         
-//        //Return -A
-//        A = 6'b001010;
-//        B = 6'b001010;
-//        fxn = 3'b010;
-//        #200;
+        //Return -A for +ve A. Output should be 6'b110110
+        A = 6'b001010;
+        B = 6'b001010;
+        fxn = 3'b010;
+        expected = 6'b110110;
+        #50;
         
-//        //Return -B
-//        A = 6'b001010;
-//        B = 6'b001010;
-//        fxn = 3'b011;
-//        #200;
+        //Return -B for +ve B. Output should be 6'b110010
+        A = 6'b001010;
+        B = 6'b001110;
+        fxn = 3'b011;
+        expected = 6'b110010;
+        #50;
         
-//        //Return A<B
-//        A = 6'b000010;
-//        B = 6'b001010;
-//        fxn = 3'b100;
-//        #200;
+        //Return -A for -ve A. Output should be 6'b010110
+        A = 6'b101010;
+        B = 6'b001010;
+        fxn = 3'b010;
+        expected = 6'b010110;
+        #50;
+        
+        //Return -B for -ve B. Output should be 6'b010010
+        A = 6'b101010;
+        B = 6'b101110;
+        fxn = 3'b011;
+        expected = 6'bb010010;
+        #50;
+        
+        //Return A<B. For +ve A and +ve B with A<B. Output should be 6'b000001
+        A = 6'b000010;
+        B = 6'b001010;
+        fxn = 3'b100;
+        expected = 6'bb000001;
+        #50;
                 
-        //Return A<B
+        //Return A<B. For +ve A and +ve B with A>B. Output should be 6'b000000
         A = 6'b000100;
         B = 6'b000010;
         fxn = 3'b100;
-        #200;
+        expected  = 6'bb000000;
+        #50;
         
-//        //Should return A!^B
-//        A = 5'b001010;
-//        B = 5'b001010;
-//        fxn = 3'b101;
-//        #200;
+        //Return A<B. For -ve A and +ve B with A<B. Output should be 6'b000001
+        A = 6'b110101;
+        B = 6'b001001;
+        fxn = 3'b100;
+        expected = 6'b000001;
         
+        //Should return A!^B. Output should be 6'b111111
+        A = 6'b001010;
+        B = 6'b001010;
+        fxn = 3'b101;
+        expected = 6'b111111;
+        #50;
+        
+        //Should return A+B. For +ve A and +ve B. Output should be 6'010100
+        A = 6'b001010;
+        B = 6'b001010;
+        fxn = 3'b110;
+        expected = 6'b010100;
+        #50; 
+        
+        //Should return A+B. For +ve A and -ve B (Essentially doing A-b). Output should be 6'000000
+        A = 6'b001010;
+        B = 6'b110110;
+        fxn = 3'b110;
+        expected = 6'b000000;
+        #50; 
+        
+        //Should return A-B. For +ve A and +ve B. Output should be 6'b000000
+        A = 6'b001010;
+        B = 6'b001010;
+        fxn = 3'b111;
+        expected = 6'b000000;
+        #50;
+        
+        //Should return A-B. For +ve A and -ve B (Essentially doing A+B). Output should be 6'b000000
+        A = 6'b001010;
+        B = 6'b111011;
+        fxn = 3'b111;
+        expected = 6'b001111;
+        #50; 
         
         
     end
