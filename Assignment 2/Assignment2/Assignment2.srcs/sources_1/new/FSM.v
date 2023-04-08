e4650df62bc5f37d1646a06c4de6b054b1ec643a
Aaron Dinesh <aarondinesh2002@gmail.com>
2023-04-08 17:38:31 +0100

    FSM now takes 19 bit number. Can pass in full output from LFSR

--- Assignment 2/Assignment2/Assignment2.srcs/sources_1/new/FSM.v
+++ Assignment 2/Assignment2/Assignment2.srcs/sources_1/new/FSM.v
@@ -1,18 +1,28 @@
 `timescale 1ns / 1ps
 
-module FSM(input num_to_check, input clk, input reset, output reg code, done);
+module FSM(input [18:0] num_to_check, input clk, output reg code, done);
     //Pattern to check for = 111111000000
     integer count_1 = 0, count_0 = 0;
     integer inputs_seen = 0;
+    reg reset;
+    integer i = 18;
+    reg num;
     localparam state_1 = 2'b01, state_0 = 2'b00, start = 2'b10, end_state = 2'b11;
     reg [1:0] current_state, next_state = start;
+    always @(num_to_check) begin
+        reset = 1;
+    end
     
     always @(posedge clk, reset) begin
         if(reset) begin
             current_state = start;
             count_1 = 0;
             count_0 = 0;
+            i = 18;
+            reset = 0;
         end else begin
+            num = num_to_check[i];
+            i = i - 1;
             current_state = next_state;
             inputs_seen = inputs_seen + 1;
         end
@@ -21,7 +31,7 @@ module FSM(input num_to_check, input clk, input reset, output reg code, done);
     always @(inputs_seen) begin
         case(current_state)
             start:
-                if(num_to_check) begin
+                if(num) begin
                     next_state  = state_1;
                     count_1 = count_1 + 1;
                 end else begin
@@ -31,7 +41,8 @@ module FSM(input num_to_check, input clk, input reset, output reg code, done);
             state_1:
                 if(count_1 == 6) begin
                     next_state = state_0;
-                end else if(num_to_check) begin
+                    count_0 = count_0 + 1;
+                end else if(num) begin
                     next_state = state_1;
                     count_1 = count_1 + 1;
                 end else begin
@@ -42,7 +53,7 @@ module FSM(input num_to_check, input clk, input reset, output reg code, done);
             state_0:
                 if(count_0 == 6 && count_1 == 6) begin
                     next_state = end_state;
-                end else if(!num_to_check) begin
+                end else if(!num) begin
                     next_state = state_0;
                     count_0 = count_0 + 1;
                 end else begin
@@ -56,6 +67,8 @@ module FSM(input num_to_check, input clk, input reset, output reg code, done);
                 count_0 = 0;
             end
         endcase
+        
+        
     end 
     
     
@@ -74,68 +87,3 @@ module FSM(input num_to_check, input clk, input reset, output reg code, done);
         endcase
     end
 endmodule
-
-
-/*
-        code = 0;
-        //Loop over the entire array
-        for(i = 18; i >=0; i = i - 1) begin
-            if(count_1 == 6 && count_0 == 0) begin
-                //All the 1s have been counted, now count 0s
-                next_state = state_0;
-            end else if(count_0 == 6 && count_1 == 6) begin
-                //WE ARE DONE OUTPUT THE RESULT
-                //We should only reach this block after we have counted six 1s and six 0s
-                code = 1;
-                count_1 = 0;
-                count_0 = 0;
-                //Set the next state to start for when the next block comes in 
-                next_state = start;
-            end else if(i == 8 && current_state == start) begin
-                //If we are at index 8 and we are still in the start state then we cannot find the code word in this block
-                code = 0;
-                count_1 = 0;
-                count_0 = 0;
-                //Set the next state to start for when the next block comes in
-                next_state = start;
-            end else begin
-            end
-        
-            //Assign the next state to the current state
-            current_state = next_state;
-            
-            if(i >= 0) begin
-                //Switch case to decide what to do based on the current state
-                case(current_state)
-                    start:
-                        //When in start we need to read a 1 before we can count the other ones
-                        if(num_to_check[i]) begin
-                            //If we get a 1, count it and set next state to state_1 (Here we count the 1s)
-                            next_state = state_1;
-                            count_1 = 1;
-                        end
-                    state_1:
-                        if(num_to_check[i]) begin
-                            //Stay in state 1 so long as we keep getting a 1 and count the 1
-                            next_state = state_1;
-                            count_1 = count_1 + 1;
-                        end else begin
-                            //If we get a 0, set the state back to start and set the counted 1s to 0
-                            next_state = start;
-                            count_1 = 0;
-                        end
-                    state_0:
-                        //If the bit is 0
-                        if(!num_to_check[i]) begin
-                            //Continute in state 0 while we are still reading 0s and increase the 0s count
-                            next_state = state_0;
-                            count_0 = count_0 + 1;
-                        end else begin
-                            //If we read a 1 then set the next state back to start and the count back to 0
-                            next_state = start;
-                            count_0 = 0;                                    
-                        end
-                 endcase
-             end
-         end
-*/
--- Assignment 2/Assignment2/Assignment2.srcs/sources_1/new/FSM_tb.v
+++ Assignment 2/Assignment2/Assignment2.srcs/sources_1/new/FSM_tb.v
@@ -1,13 +1,12 @@
 `timescale 1ns / 1ps
 module FSM_tb();
     reg clk;
-    reg [18:0] num_to_test = 19'b0000000110111010000;
-    reg input_to_dut;
+    reg [18:0] num_to_test = 19'b1111110000000000000;
     reg reset;
     integer i = 0;
     integer pos = 0;
     wire out, done;
-    FSM dut(.clk(clk), .num_to_check(input_to_dut), .reset(reset), .code(out), .done(done));
+    FSM dut(.clk(clk), .num_to_check(num_to_test), .code(out), .done(done));
     
     initial begin
         clk = 1'b0;
@@ -16,12 +15,11 @@ module FSM_tb();
         end
      end
      
-     always @(posedge clk) begin
-        pos = 19 - (i + 1);
-        input_to_dut = num_to_test[pos];
-        i = (i + 1) % 18;
-        #1;
-     end
+    always @(posedge done) begin
+        num_to_test  = num_to_test >> 1;
+        #10;
+    end
+
     
 
 endmodule
